c456a33aadcf3c0a72bd7f68eac5101c83e98ade
Josh <jrodwod@gmail.com>
2020-05-26 04:50:04 -0400

    tidying up

--- commands/server/play.js
+++ commands/server/play.js
@@ -7,30 +7,19 @@ module.exports = {
 	run: (bot, msg, args) => {
 
 		const vc = msg.member.voice.channel;
-
-		if (!vc) {
-			return;
-		}
-
 		const [user, sound] = args;
 		const baseDir = dirname(process.mainModule.filename);
 		const file = join(baseDir, `/sounds/${user}/${sound}.mp3`);
-		
-		if (!existsSync(file)) {
-			console.log('could not find file')
-			return;
-		}
 
-		playFile(vc, file);
+		if (!vc ||!existsSync(file)) return;
 
-		async function playFile(vc, file) {
-			const connection = await vc.join();
-			const dispatcher = connection.play(file);
-			dispatcher.on('finish', () => {
-				vc.leave();
-				bot.destroy();
-				bot.login(process.env.BOT_TOKEN);
-			});
-		}
+		const connection = await vc.join();
+		const dispatcher = connection.play(file);
+		dispatcher.on('finish', () => {
+			vc.leave();
+			bot.destroy();
+			bot.login(process.env.BOT_TOKEN);
+		});
+
 	}
 }
